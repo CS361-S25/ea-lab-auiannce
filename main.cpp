be2d56ab15e825a97602011d179b46f313a344d5
Aviannce Euwing <aviannceeuwing@Aviannces-MacBook-Air-2.local>
2025-04-04 09:22:34 -0500

    started on my ea

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 #include "Empirical/include/emp/math/Random.hpp"
+class Organism {
+    public:
+        double trait;
+        Organism(double t) : trait(t) {}
+
+        Organism* reproduce() {
+           Organism* child = new Organism(trait);
+           return child;
+        }
+};
 
 int main() {
-
+    emp::vector<Organism> population;
+    
+    for (int i = 0; i < 100; ++i) {
+        population.push_back(*new Organism(0.5));
+    }
+    std::cout << "Population trait: " << population[0].trait << std::endl;
+    
 }
+    
